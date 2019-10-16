e1e58ba899cfd108c9debc91b5f2374adfaf4cb4
klema21 <klema2727@gmail.com>
2019-10-16 14:15:15 +0300

    Rewrite a little files.

--- GUI.py
+++ GUI.py
@@ -1,35 +1,51 @@
-"""from tkinter import *
+from tkinter import *
+import main
 
 class Application(Frame):
+    def __init__(self, master):
+        super(Application, self).__init__(master)
+        self.grid()
+        self.board = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+        self.counter = 0
+        #self.create_widget()
+        #self.on_click()
+
+        def create_widget(self):
+            self.bttn = Button(self)
+            self.bttn["Text"] = "Run"
+            self.bttn["Command"] = self.run
+            self.bttn.grid()
+
+        def run(self):
+            result = main.main(board)
+            return result
+
+        def on_click(self, i, j, event):
+            global counter
+            color = "red" if counter%2 else "black"
+            event.widget.config(bg=color)
+            board[i][j] = color
+            board[i][j] = 1
+            counter += 1
+
+        for i, row in enumerate(self.board):
+            for j, column in enumerate(row):
+                L = Label(root, text='    ', bg="grey")
+                L.grid(row=i+1, column=j+1)
+                L.bind('<Button-1>', lambda e,i=i,j=j: on_click(i,j,e))
+
 
 root = Tk()
 root.title("Pathfinding Visualization")
-root.geometry("300x250")
-
+root.geometry("300x300")
 app = Application(root)
-root.mainloop()
-"""
-
-import tkinter as tk
-
-board = [ [None]*10 for _ in range(10) ]
-
-counter = 0
-
-root = tk.Tk()
-
-def on_click(i,j,event):
-    global counter
-    color = "red" if counter%2 else "black"
-    event.widget.config(bg=color)
-    board[i][j] = color
-    counter += 1
-
-
-for i,row in enumerate(board):
-    for j,column in enumerate(row):
-        L = tk.Label(root,text='    ',bg='grey')
-        L.grid(row=i,column=j)
-        L.bind('<Button-1>',lambda e,i=i,j=j: on_click(i,j,e))
-
 root.mainloop()
