33a471cb0073cd7aa1ee87310d72bf233ac01bf0
klema21 <klema2727@gmail.com>
2019-10-16 11:37:07 +0300

    Choose Tkinter to create GUI app for visualization.

---
+++ GUI.py
@@ -0,0 +1,35 @@
+"""from tkinter import *
+
+class Application(Frame):
+
+root = Tk()
+root.title("Pathfinding Visualization")
+root.geometry("300x250")
+
+app = Application(root)
+root.mainloop()
+"""
+
+import tkinter as tk
+
+board = [ [None]*10 for _ in range(10) ]
+
+counter = 0
+
+root = tk.Tk()
+
+def on_click(i,j,event):
+    global counter
+    color = "red" if counter%2 else "black"
+    event.widget.config(bg=color)
+    board[i][j] = color
+    counter += 1
+
+
+for i,row in enumerate(board):
+    for j,column in enumerate(row):
+        L = tk.Label(root,text='    ',bg='grey')
+        L.grid(row=i,column=j)
+        L.bind('<Button-1>',lambda e,i=i,j=j: on_click(i,j,e))
+
+root.mainloop()
