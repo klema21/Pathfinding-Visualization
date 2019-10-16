d38af1aa706bdc41a105a45a67a27f0a82022f47
klema21 <klema2727@gmail.com>
2019-10-16 15:19:50 +0300

    Did the working concept with Python3 and Tkinter.

--- GUI.py
+++ GUI.py
@@ -1,51 +1,34 @@
-from tkinter import *
 import main
+from tkinter import *
 
-class Application(Frame):
-    def __init__(self, master):
-        super(Application, self).__init__(master)
-        self.grid()
-        self.board = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
-        self.counter = 0
-        #self.create_widget()
-        #self.on_click()
+board = [ [0]*10 for _ in range(10) ]
 
-        def create_widget(self):
-            self.bttn = Button(self)
-            self.bttn["Text"] = "Run"
-            self.bttn["Command"] = self.run
-            self.bttn.grid()
-
-        def run(self):
-            result = main.main(board)
-            return result
-
-        def on_click(self, i, j, event):
-            global counter
-            color = "red" if counter%2 else "black"
-            event.widget.config(bg=color)
-            board[i][j] = color
-            board[i][j] = 1
-            counter += 1
-
-        for i, row in enumerate(self.board):
-            for j, column in enumerate(row):
-                L = Label(root, text='    ', bg="grey")
-                L.grid(row=i+1, column=j+1)
-                L.bind('<Button-1>', lambda e,i=i,j=j: on_click(i,j,e))
-
+counter = 0
 
 root = Tk()
 root.title("Pathfinding Visualization")
 root.geometry("300x300")
-app = Application(root)
+
+def on_click(i,j,event):
+    global counter
+    color = "red" if counter%2 else "black"
+    event.widget.config(bg=color)
+    #board[i][j] = color
+    board[i][j] = 1
+    counter += 1
+
+def run():
+    result = main.main(board)
+    return result
+
+for i,row in enumerate(board):
+    for j,column in enumerate(row):
+        L = Label(root,text='    ',bg='grey')
+        L.grid(row=i+1,column=j+1)
+        L.bind('<Button-1>',lambda e,i=i,j=j: on_click(i,j,e))
+
+app = Frame(root)
+app.grid()
+bttn = Button(text="Run", command=run)
+bttn.grid(row=0, column=0)
 root.mainloop()
